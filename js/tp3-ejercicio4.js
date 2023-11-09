7d228b81259d1079d5904b32e71f4ed5e9b50bb0
Juan Pablo Toledo <toledo.pablo87@gmail.com>
2023-11-08 23:32:05 -0300

    actualizamos ejercicio4 del tp3

--- js/tp3-ejercicio4.js
+++ js/tp3-ejercicio4.js
@@ -1,7 +1,16 @@
-function ParOImpar(numero = 0){
-    return (numero % 2 !== 0)
-    document.write("el numero "+numero+ " es impar") ;
+function ParOImpar(numero){
+    while (isNaN(numero)){
+        numero = parseInt(prompt(`Ingrese un numero: `))
+    }
+    if(numero %2 ==0){
+        document.write(`El numero ${numero} es par`);
 
+    }else{
+        document.write(`El numero ${numero} es impar`);
+    }
 
 }
+
+ParOImpar();
+
 
